c2bbb3139a822478616098b850dad8096aa5ada4
DunkelDeutscherRauch <SError@gmx.de>
2024-01-15 17:53:47 +0100

    minor Updates

--- src/main/kotlin/Enchantress.kt
+++ src/main/kotlin/Enchantress.kt
@@ -1,5 +1,11 @@
 class Enchantress(name: String = "") : Hero(name, healthPoints = 250) {
 
+    var attackOne: IntRange = (0..0)
+    var attackTwo: Int = 0
+    var attackThree: IntRange = (0..0)
+    var attackFour: IntRange = (0..0)
+    var attackFive: Int = 10000
+    var healingItem: Int = 0
 
 
 }
